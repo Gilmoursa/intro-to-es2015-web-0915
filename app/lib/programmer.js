77851ee86f149c6bbc4ea0ecbe1a1779e09e680f
Gilmoursa <gilmoursa@gmail.com>
2015-11-20 11:54:33 -0500

    update after code walkthrough

--- app/lib/programmer.js
+++ app/lib/programmer.js
@@ -1,5 +1,15 @@
-class Programmer {
-	constructor(name,language="ruby"){
+import Ember from 'ember';
+
+export var bestLanguages = ["Ruby", "Elixir"];
+
+export function addJavaScript(languages){
+	let newArray = Ember.copy(languages);
+	newArray.push("JavaScript");
+	return newArray;
+}
+
+export default class Programmer {
+	constructor(name,language="Ruby"){
 		this.name = name;
 		this.language = language;
 	}
@@ -7,9 +17,3 @@ class Programmer {
 		return `${this.name}: ${this.language.toUpperCase()} IS THE BEST LANGUAGE EVER`;
 	}
 }
-export var bestLanguages = ["Ruby", "Elixir"];
-export function addJavaScript(languages){
-	var newList = languages + [", JavaScript"];
-	return newList;
-}
-export default Programmer;
